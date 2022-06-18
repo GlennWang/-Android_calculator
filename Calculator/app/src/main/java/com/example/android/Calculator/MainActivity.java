4e5ffa98ea7c3ddcbad49470a9a55947d6e208b0
Glenn Wang <abc20200130abc@gmail.com>
2022-06-18 17:06:40 +0800

    update

---
+++ Calculator/app/src/main/java/com/example/android/Calculator/MainActivity.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.Calculator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.LinkedList;
+
+/**
+ * Implements a basic RecyclerView that displays a list of generated words.
+ * - Clicking an item marks it as clicked.
+ * - Clicking the fab button adds a new word to the list.
+ */
+public class MainActivity extends AppCompatActivity {
+    private String[] name = new String[5];
+    private final LinkedList<String> mWordList = new LinkedList<>();
+    private final LinkedList<String> mWordList2 = new LinkedList<>();
+
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Put initial data into the word list.
+        name[0] = "普通計算機";
+        name[1] = "進制計算機";
+        name[2] = "時間計算機";
+        name[3] = "單身計算機";
+        name[4] = "排卵期計算機";
+        for (int i = 0; i < 5; i++) {
+            mWordList.addLast(name[i]);
+        }
+        mWordList2.addLast("具有普通計算功能的計算機");
+        mWordList2.addLast("進行進制轉換的計算機");
+        mWordList2.addLast("計時工具");
+        mWordList2.addLast("計算你單身了幾天的計算機");
+        mWordList2.addLast("計算你的排卵期的計算機");
+
+
+        // Create recycler view.
+        mRecyclerView = findViewById(R.id.recyclerview);
+        // Create an adapter and supply the data to be displayed.
+        mAdapter = new WordListAdapter(this, mWordList, mWordList2);
+        // Connect the adapter with the recycler view.
+        mRecyclerView.setAdapter(mAdapter);
+        // Give the recycler view a default layout manager.
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    /**
+     * Inflates the menu, and adds items to the action bar if it is present.
+     *
+     * @param menu Menu to inflate.
+     * @return Returns true if the menu inflated.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    /**
+     * Handles app bar item clicks.
+     *
+     * @param item Item clicked.
+     * @return True if one of the defined items was clicked.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        // This comment suppresses the Android Studio warning about simplifying
+        // the return statements.
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
